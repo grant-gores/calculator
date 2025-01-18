755a0829302f23460874107e77896291a94a2ec3
grant-gores <140012939+grant-gores@users.noreply.github.com>
2025-01-18 12:51:07 -0600

    Create math calcs and operate function
    Created basic math calcs: add, sub, multiply, divide.
    Then created an operate function so you can call any of these
    functions and two numbers and get the result.

---
+++ script.js
@@ -0,0 +1,45 @@
+// Create calculator functions
+function add(a, b){
+    let result = 0;
+    return result = a + b;
+};
+
+function subtract(a, b){
+    let result = 0;
+    return result = a - b;
+};
+
+function multiply(a, b){
+    let result = 0;
+    return result = a * b;
+};
+
+function divide(a, b){
+    let result = 0;
+    return result = a/b;
+};
+
+// three variables for an operation
+let firstNumber = 0;
+let operator = "";
+let secondNumber = 0;
+
+function operate(operator, firstNumber, secondNumber){
+    if (operator === "+"){
+        return add(firstNumber,secondNumber);
+    }
+    else if(operator === "-"){
+        return subtract(firstNumber,secondNumber);
+    }
+    else if(operator === "*"){
+        return multiply(firstNumber,secondNumber);
+    }
+    else if(operator === "/"){
+        return divide(firstNumber,secondNumber);
+    }   
+    else{
+        return "Enter a valid operator ('+', '-', '*', or '/')";
+    }
+};
+
+
