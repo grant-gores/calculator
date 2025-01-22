c64bdd6f2b73dc5bd174148e27a1b2a2cd311fa7
grant-gores <140012939+grant-gores@users.noreply.github.com>
2025-01-21 19:46:14 -0600

    Create functioning calculator
    Successfully got the basics of the calculator to work.
    Needed to add a "awaitingSecondNumber" variable to tell
    the code to either start from scratch or append
    to the display.

--- script.js
+++ script.js
@@ -1,44 +1,102 @@
 // Create calculator functions
-function add(a, b){
-    let result = 0;
-    return result = a + b;
-};
+function add(a, b) {
+    return a + b;
+}
 
-function subtract(a, b){
-    let result = 0;
-    return result = a - b;
-};
+function subtract(a, b) {
+    return a - b;
+}
 
-function multiply(a, b){
-    let result = 0;
-    return result = a * b;
-};
+function multiply(a, b) {
+    return a * b;
+}
 
-function divide(a, b){
-    let result = 0;
-    return result = a/b;
-};
+function divide(a, b) {
+    return a / b;
+}
 
-// three variables for an operation
-let firstNumber = 0;
-let operator = "";
-let secondNumber = 0;
-
-function operate(firstNumber, operator, secondNumber){
-    if (operator === "+"){
-        return add(firstNumber,secondNumber);
-    }
-    else if(operator === "-"){
-        return subtract(firstNumber,secondNumber);
-    }
-    else if(operator === "*"){
-        return multiply(firstNumber,secondNumber);
-    }
-    else if(operator === "/"){
-        return divide(firstNumber,secondNumber);
-    }   
-    else{
-        return "Enter a valid operator ('+', '-', '*', or '/')";
+// Function to perform operation
+function operate(firstNumber, operator, secondNumber) {
+    switch (operator) {
+        case '+':
+            return add(firstNumber, secondNumber);
+        case '-':
+            return subtract(firstNumber, secondNumber);
+        case '*':
+            return multiply(firstNumber, secondNumber);
+        case '/':
+            return divide(firstNumber, secondNumber);
+        default:
+            return null;
     }
 }
 
+// Variables to store the state
+let firstNumber = null;
+let operator = null;
+let secondNumber = null;
+let awaitingSecondNumber = false;
+let currentValue = "0";
+const MAX_DIGITS = 9;
+
+// Display element
+const calcDisplay = document.querySelector("#calc");
+
+// Update display function
+function updateDisplay(value) {
+    calcDisplay.innerText = value;
+}
+
+// Event listeners for operand buttons
+document.querySelectorAll(".operand").forEach(button => {
+    button.addEventListener("click", () => {
+        if (awaitingSecondNumber) {
+            currentValue = button.innerText;
+            awaitingSecondNumber = false;
+        } else {
+            if (currentValue === "0") {
+                currentValue = button.innerText;
+            } else if (currentValue.length < MAX_DIGITS) {
+                currentValue += button.innerText;
+            }
+        }
+        updateDisplay(currentValue);
+    });
+});
+
+// Event listeners for operator buttons
+document.querySelectorAll(".operator").forEach(button => {
+    button.addEventListener("click", () => {
+        if (firstNumber === null) {
+            firstNumber = parseFloat(currentValue);
+        } else if (operator) {
+            secondNumber = parseFloat(currentValue);
+            firstNumber = operate(firstNumber, operator, secondNumber);
+            updateDisplay(firstNumber);
+        }
+        operator = button.innerText;
+        awaitingSecondNumber = true;
+    });
+});
+
+// Event listener for equals button
+document.querySelector(".equals").addEventListener("click", () => {
+    if (firstNumber !== null && operator !== null && !awaitingSecondNumber) {
+        secondNumber = parseFloat(currentValue);
+        const result = operate(firstNumber, operator, secondNumber);
+        updateDisplay(result);
+        firstNumber = result;
+        operator = null;
+        awaitingSecondNumber = true;
+    }
+});
+
+// Event listener for clear button
+document.querySelector(".clear").addEventListener("click", () => {
+    firstNumber = null;
+    operator = null;
+    secondNumber = null;
+    awaitingSecondNumber = false;
+    currentValue = "0";
+    updateDisplay(currentValue);
+});
